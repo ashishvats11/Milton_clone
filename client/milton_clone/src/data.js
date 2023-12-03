f9a81afc3336cab003129f0e5ebe0ed0b8a0bd83
Ashish Vats <80643755+ashishvats11@users.noreply.github.com>
2023-12-03 13:56:52 +0530

    created data.json

---
+++ client/milton_clone/src/data.js
@@ -0,0 +1,30 @@
+[
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    },
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    },
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    },
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    },
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    },
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    },
+    {
+        "testimonial": "loremfjeneee fe dfdf dfdfd",
+        "author": "ashish"
+    }
+]
